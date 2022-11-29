5d2dd1c7140fc91af2ffae4a9a7f61b43e4bc2f0
Matias Asis <matiasis.90@gmail.com>
2022-11-28 21:22:51 -0300

    improve scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Overwriting the sign_out redirect path method
+  # Default URL after sign out
   def after_sign_out_path_for(_user)
     '/auth/sign_in'
   end
 
-  # Overwriting the sign_in redirect path method
+  # Default url when sign in
   def after_sign_in_path_for(_user)
-    # debugger
     '/users'
   end
 
