30827272e090e4c60994aa72f26c041975386db5
Matias Asis <matiasis.90@gmail.com>
2022-11-28 22:01:14 -0300

    users fix

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_220132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
