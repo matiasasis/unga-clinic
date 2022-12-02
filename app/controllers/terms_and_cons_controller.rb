ae6d0eb5aa8b51423dd0b3ce47eda7763646e806
MatiasChao <matias.chao@despegar.com>
2022-12-02 00:14:53 -0300

    changes in terms and cons view

--- app/controllers/terms_and_cons_controller.rb
+++ app/controllers/terms_and_cons_controller.rb
@@ -1,34 +1,15 @@
 # frozen_string_literal: true
 
 class TermsAndConsController < ApplicationController
-  before_action :set_terms_and_con, only: %i[show edit update destroy]
+  before_action :set_terms_and_con, only: %i[edit update]
 
   # GET /terms_and_cons
   def index
-    @terms_and_cons = TermsAndCon.paginate(page: params[:page], per_page: 12)
-  end
-
-  # GET /terms_and_cons/1
-  def show; end
-
-  # GET /terms_and_cons/new
-  def new
-    @terms_and_con = TermsAndCon.new
+    @terms_and_cons = TermsAndCon.first_or_create
   end
 
   # GET /terms_and_cons/1/edit
   def edit; end
-
-  # POST /terms_and_cons
-  def create
-    @terms_and_con = TermsAndCon.new(terms_and_con_params)
-
-    if @terms_and_con.save
-      redirect_to terms_and_cons_url, notice: "#{I18n.t('activerecord.models.terms_and_con.one')} creado exitosamente."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
 
   # PATCH/PUT /terms_and_cons/1
   def update
@@ -40,17 +21,11 @@ class TermsAndConsController < ApplicationController
     end
   end
 
-  # DELETE /terms_and_cons/1
-  def destroy
-    @terms_and_con.destroy
-    redirect_to terms_and_cons_url, notice: "#{I18n.t('activerecord.models.terms_and_con.one')} eliminado exitosamente."
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_terms_and_con
-    @terms_and_con = TermsAndCon.find(params[:id])
+    @terms_and_con = TermsAndCon.first_or_create
   end
 
   # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :professionals, except: [:show]
   resources :users, except: [:show]
   resources :faqs, except: [:show]
-  resources :terms_and_cons
+  resources :terms_and_cons, only: [:index, :edit, :update]
   devise_for :users, path: 'auth'
 end
