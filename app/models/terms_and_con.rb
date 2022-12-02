7fec2044fe95d426eb500df8b03fb5239b802a26
MatiasChao <matias.chao@despegar.com>
2022-12-02 00:32:43 -0300

    comment model

--- app/models/terms_and_con.rb
+++ app/models/terms_and_con.rb
@@ -7,3 +7,18 @@ class TermsAndCon < ApplicationRecord
     'Términos y Condiciones'
   end
 end
+
+# == Schema Information
+#
+# Table name: terms_and_cons
+#
+#  id         :bigint           not null, primary key
+#  deleted_at :datetime
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_terms_and_cons_on_deleted_at  (deleted_at)
+#
