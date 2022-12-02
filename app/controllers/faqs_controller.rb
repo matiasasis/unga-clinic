18c616dde804538ebb0ba68f6e37d46dd67a4d1c
MatiasChao <matias.chao@despegar.com>
2022-12-02 00:26:33 -0300

    change icon and fix schema

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FaqsController < ApplicationController
-  before_action :set_faq, only: %i[show edit update destroy]
+  before_action :set_faq, only: %i[edit update destroy]
 
   # GET /faqs
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_012824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "address"
+  create_table "faqs", force: :cascade do |t|
+    t.string "title"
+    t.string "answer"
+    t.boolean "show"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_faqs_on_deleted_at"
   end
 
   create_table "professionals", force: :cascade do |t|
