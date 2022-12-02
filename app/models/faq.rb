f96c6f6988a7361f0701f5d8655e5972feca7724
Matias Asis <matiasis.90@gmail.com>
2022-12-02 14:27:47 -0300

    fixes

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -4,6 +4,23 @@ class Faq < ApplicationRecord
   acts_as_paranoid
 
   def to_s
-    name
+    title
   end
 end
+
+# == Schema Information
+#
+# Table name: faqs
+#
+#  id         :bigint           not null, primary key
+#  answer     :string
+#  deleted_at :datetime
+#  show       :boolean
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_faqs_on_deleted_at  (deleted_at)
+#
